bf71b1684f3cbf67006d24da7e2b4a7e0eb815e1
vaibhavadwani <vaibhav.adwani@tigeranalytics.com>
2022-07-19 12:23:37 +0530

    updated tota_sales.py

--- assignment2/resources/total_sales.py
+++ assignment2/resources/total_sales.py
@@ -4,25 +4,12 @@ import sqlite3
 
 
 class Total_sales(Resource):
-    # parser = reqparse.RequestParser()
-
-    # parser.add_argument(
-    #     "name", type=str, required=True, help="this field cannot be blank"
-    # )
-    # parser.add_argument(
-    #     "value", type=str, required=True, help="this field cannot be blank"
-    # )
-
     def get(self):
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
-        query = "SELECT sale_amount FROM sales_data"
+        query = "SELECT SUM(sale_amount) FROM sales_data"
         result = cursor.execute(query)
         row = result.fetchall()
         connection.close()
-        # row is a list of tuples
-        data = ()
-        for i in range(0, len(row)):
-            data = data + row[i]
-        total_sales = sum(data)
-        return {"total_sales": total_sales}
+
+        return {"total_sales": row[0][0]}
