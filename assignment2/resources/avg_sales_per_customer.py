16e13cccaad506f9535c2bfcce7174af2bee7593
vaibhavadwani <vaibhav.adwani@tigeranalytics.com>
2022-07-19 12:10:42 +0530

    updated avg_Sales_per_customer.py

--- assignment2/resources/avg_sales_per_customer.py
+++ assignment2/resources/avg_sales_per_customer.py
@@ -7,26 +7,17 @@ class Average_sales(Resource):
     def get(self):
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
-        query1 = "SELECT sale_amount FROM sales_data"
-        # query2 = "SELECT DISTINCT user_id FROM sales_data"
+        query1 = "SELECT user_id, SUM(sale_amount) FROM sales_data GROUP BY user_id"
+
         result1 = cursor.execute(query1)
-        # result2 = cursor.execute(query2)
+
         row1 = result1.fetchall()
-        # row2 = result2.fetchall()
+
         connection.close()
 
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-        query2 = "SELECT DISTINCT user_id FROM sales_data"
-        result2 = cursor.execute(query2)
-        row2 = result2.fetchall()
-        connection.close()
-
-        # row is a list of tuples
-        data = ()
+        data = 0
         for i in range(0, len(row1)):
-            data = data + row1[i]
-        total_sales = sum(data)
+            data = data + row1[i][1]
 
-        unique_visitors = len(row2)
-        return {"average_sales_per_customer": (total_sales / unique_visitors)}
+        unique_visitors = len(row1)
+        return {"average_sales_per_customer": data / unique_visitors}
